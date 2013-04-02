5dfb49cc06827a6c442cd8156a5e5a3970e4aa16
Pete <pete.bunch@gmail.com>
2013-04-03 00:00:40 +0100

    Moved into one file. Separated database and ui functionality into separate classes.

--- accountable.py
+++ accountable.py
@@ -26,40 +26,114 @@ import sys
 
 
 
-class Account:
+class DatabaseHandler:
     
     def __init__(self, con):
         self.con = con
         self.cur = con.cursor()
         self.cur.execute("""CREATE TABLE IF NOT EXISTS people(
-                    id INTEGER PRIMARY KEY,
+                    idx INTEGER PRIMARY KEY,
                     name TEXT,
                     team TEXT,
                     notes TEXT
                     starting_balance REAL
                         )""")
         self.cur.execute("""CREATE TABLE IF NOT EXISTS events(
-                    id INTEGER PRIMARY KEY,
+                    idx INTEGER PRIMARY KEY,
                     name TEXT,
                     date REAL,
                     cost REAL,
                     notes TEXT
                         )""")
         self.cur.execute("""CREATE TABLE IF NOT EXISTS p_e(
-                    id INTEGER PRIMARY KEY,
+                    idx INTEGER PRIMARY KEY,
                     person INTEGER,
                     event INTEGER,
                     ratio REAL
                         )""")
 
 
+    def check_person_exists(self,name):
+        self.cur.execute("SELECT COUNT(*) FROM people WHERE name=?", (name,))
+        tmp = self.cur.fetchone()[0]
+        if tmp == 0:
+            exist = False
+        else:
+            exist = True
+        return exist
 
 
+    def check_event_exists(self,name):
+        self.cur.execute("SELECT COUNT(*) FROM events WHERE name=?", (name,))
+        tmp = self.cur.fetchone()[0]
+        if tmp == 0:
+            exist = False
+        else:
+            exist = True
+        return exist
+
+
+    def insert_person(self, name, team, note):
+        self.cur.execute("INSERT INTO people(name,team,notes) VALUES(?,?,?);", (name,team,note))
+        self.con.commit()
+        idx = self.cur.lastrowid
+        return idx
+
+
+    def insert_event(self, name, date, cost, note):
+        self.cur.execute("INSERT INTO events(name,date,cost,notes) VALUES(?,?,?,?);", (name,date,cost,note))
+        self.con.commit()
+        idx = self.cur.lastrowid
+        return idx
+
+
+    def link_people_to_event(self, event_idx, people_list):
+        for pp in people_list:
+            self.cur.execute("INSERT INTO p_e(person, event) VALUES(?,?);", (pp,event_idx))
+        self.con.commit()
+
+
+    def retrieve_people(self, field=None, match=None):
+        if field is None:
+            self.cur.execute("SELECT * FROM people")
+        else:
+            instruct = "SELECT * FROM people WHERE %s=?" % field
+            self.cur.execute(instruct, (match,))
+        people = self.cur.fetchall()
+        return people
+
+
+    def retrieve_events(self, field=None, match=None):
+        if field is None:
+            self.cur.execute("SELECT * FROM events")
+        else:
+            instruct = "SELECT * FROM events WHERE %s=?" % field
+            self.cur.execute(instruct, (match,))
+        events = self.cur.fetchall()
+        return events
+
+    def retrieve_events_by_person(self, person_idx):
+        self.cur.execute("SELECT * FROM p_e WHERE person=?", (person_idx,))
+        link_list = self.cur.fetchall()
+        events = []
+        for ll in link_list:
+            event_idx = ll["event"]
+            self.cur.execute("SELECT * FROM events WHERE idx=?",(event_idx,))
+            event = self.cur.fetchone()
+            events.append(event)
+        return events
+
+
+
+
+
+class CmdLineUI:
+
+    def __init__(self, db):
+        self.db = db
 
     def main(self):
-        
         while True:
-            
             ans = raw_input("""What would you like to do?
 vp = View people list
 ve = View event list
@@ -73,155 +147,98 @@ s  = Statement
 q  = quit\n:""")
         
             if ans == "ap":
-                self.ui_add_person()
+                self.cb_add_person()
             if ans == "ae":
-                self.ui_add_event()
+                self.cb_add_event()
             if ans == "vp":
-                self.ui_view_people()
+                self.cb_view_people()
             if ans == "ve":
-                self.ui_view_events()
+                self.cb_view_events()
             if ans == "s":
-                self.ui_statement()
+                self.cb_statement()
             if ans == "q":
                 sys.exit()
 
 
+    ### CALLBACK METHODS ###
 
-
-
-
-    def ui_add_person(self):
+    def cb_add_person(self):
         print "Creating a new person..."
         exist = True
         while exist:
             name = raw_input("What's their name?   :")
-            exist = self.db_check_person_exists(name)
+            exist = self.db.check_person_exists(name)
             if exist:
                 print "   That person already exists."
         team = raw_input("What team/crew/group are they in?   :")
         note = raw_input("Anything to say about them?   :")
-        
-        self.cur.execute("INSERT INTO people(name,team,notes) VALUES(?,?,?);", (name,team,note))
-        self.con.commit()
+        self.db.insert_person(name, team, note)
 
 
-
-
-
-    def ui_add_event(self):
+    def cb_add_event(self):
         print "Creating an event..."
         exist = True
         while exist:
             name = raw_input("What was the event?   :")
-            exist = self.db_check_event_exists(name)
+            exist = self.db.check_event_exists(name)
             if exist:
-                print "That event already exists."
+                print "   That event already exists."
         date = raw_input("What was the date?   :")
         sys.stdout.write(u'How much did it cost?   :£')
         cost = raw_input("")
         note = raw_input("Any notes?   :")
+        event_idx = self.db.insert_event(name,date,cost,note)
         
-        self.cur.execute("INSERT INTO events(name,date,cost,notes) VALUES(?,?,?,?);", (name,date,cost,note))
-        event_id = self.cur.lastrowid
-        
-        # Display the possible people
-        self.cur.execute("SELECT * FROM people")
-        rows = self.cur.fetchall()
-        for rr in rows:
-            print "%s %s" % (rr["id"], rr["name"])
-
+        # Now get a list of people contributing
+        self.display_people()
         tmp = raw_input("Enter a list of ids seperated by spaces for the people who attended this event:")
-        id_list = map(int, tmp.split())
+        people_list = map(int, tmp.split())
+        # THIS NEEDS SOME MORE PARSING TO MAKE SURE THEY'RE VALID, EXISTING IDXS
+        self.db.link_people_to_event(event_idx, people_list)
 
-        for pp in id_list:
-            self.cur.execute("INSERT INTO p_e(person, event) VALUES(?,?);", (pp,event_id))
-            # THIS NEEDS SOME PARSING TO MAKE SURE THEY'RE VALID, EXISTING IDS
 
-        self.con.commit()
-
-        
-
-        
-
-    def ui_view_people(self):
+    def cb_view_people(self):
         self.display_people()
         raw_input("Press enter to continue")
 
 
-
-
-
-    def ui_view_events(self):
+    def cb_view_events(self):
         self.display_events()
         raw_input("Press enter to continue")
 
 
-
-
-
-    def ui_statement(self):
+    def cb_statement(self):
         self.display_people()
-        id = raw_input("Enter the id of the person whose statement you want:")
-        self.cur.execute("SELECT * FROM people WHERE id=?",id)
-        subject = self.cur.fetchone()
-        print "Retrieving statement for %s." % subject["name"]
-        self.cur.execute("SELECT * FROM p_e WHERE person=?",id)
-        e_list = self.cur.fetchall()
-        print "id | Name                 | Date       | Cost       | Notes"
-        for ee in e_list:
-            e_id = ee["event"]
-            self.cur.execute("SELECT * FROM events WHERE id=?",(e_id,))
-            event = self.cur.fetchone()
-            print "%2s | %-20s | %-10s | %-10s | %s" % (event["id"], event["name"], event["date"], event["cost"], event["notes"])
+        idx = raw_input("Enter the id of the person whose statement you want:")
+        subject = self.db.retrieve_people('idx', idx)
+        subject_idx = subject[0]["idx"]
+        print "Retrieving statement for %s." % subject[0]["name"]
+        events = self.db.retrieve_events_by_person(subject_idx)
+        for ee in events:
+            print "%2s | %-20s | %-10s | %-10s | %s" % (ee["idx"], ee["name"], ee["date"], ee["cost"], ee["notes"])
         raw_input("Press enter to continue")
-        
 
-
-
-    ### DATABASE METHODS ###
-
-    def db_check_person_exists(self,name):
-        self.cur.execute("SELECT COUNT(*) FROM people WHERE name=?", (name,))
-        tmp = self.cur.fetchone()[0]
-        if tmp == 0:
-            exist = False
-        else:
-            exist = True
-        return exist
-
-    def db_check_event_exists(self,name):
-        self.cur.execute("SELECT COUNT(*) FROM events WHERE name=?", (name,))
-        tmp = self.cur.fetchone()[0]
-        if tmp == 0:
-            exist = False
-        else:
-            exist = True
-        return exist
-        
 
     ### DISPLAY METHODS ###
     
-    def display_people(self):
-        # Display a list of everyone in the database
-        print "Here's a list of everyone in the database"
-        self.cur.execute("SELECT * FROM people")
-        rows = self.cur.fetchall()
-        print "id | Name                 | Team      | Notes"
-        for rr in rows:
-            print "%2s | %-20s | %-10s | %s" % (rr["id"], rr["name"], rr["team"], rr["notes"])
+    def display_people(self, field=None, match=None):
+        people = self.db.retrieve_people(field, match)
+        if people is None:
+            print("Noone matching")
+            return
+        print("id | Name                 | Team      | Notes")
+        for pp in people:
+            print ("%2s | %-20s | %-10s | %s" % (pp["idx"], pp["name"], pp["team"], pp["notes"]))
 
 
-
-
-
-    def display_events(self):
-        # Display a list of all the events
-        print "Here's a list of all the events in the database"
-        self.cur.execute("SELECT * FROM events")
-        rows = self.cur.fetchall()
+    def display_events(self, field=None, match=None):
+        events = self.db.retrieve_events(field, match)
+        if events is None:
+            print("No events matching")
+            return
         print "id | Name                 | Date       | Cost       | Notes"
-        for rr in rows:
-            print "%2s | %-20s | %-10s | %-10s | %s" % (rr["id"], rr["name"], rr["date"], rr["cost"], rr["notes"])
+        for ee in events:
+            print "%2s | %-20s | %-10s | %-10s | %s" % (ee["idx"], ee["name"], ee["date"], ee["cost"], ee["notes"])
 
 
 
@@ -229,7 +246,7 @@ q  = quit\n:""")
 
 
 
-
+### MAIN CODE ###
 
 # See if we got an argument
 if len(sys.argv) == 1:
@@ -251,8 +268,9 @@ con = lite.connect(filename)
 with con:
     con.row_factory = lite.Row
     cur = con.cursor()
-    acc = Account(con)
-    acc.main()
+    db = DatabaseHandler(con)
+    ui = CmdLineUI(db)
+    ui.main()
 
 sys.exit()
 
